9820eccf10921adfe4f1a5418e3a38f2510a0855
Eric-Philippe <ericphlpp@gmail.com>
2024-07-06 01:00:17 +0200

    Migrate Menu from React to React.Bootstrap

--- hillcrest-react/src/modules/core/components/Header.tsx
+++ hillcrest-react/src/modules/core/components/Header.tsx
@@ -1,153 +1,161 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import LanguageToggle from "../design/LanguageToggle";
 import "./styles/Header.css";
+import { Container, Navbar } from "react-bootstrap";
 
 const Header: React.FC = () => {
-  const [isSmallScreen, setIsSmallScreen] = React.useState(
-    window.innerWidth < 992
-  );
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 992);
+  const { t } = useTranslation();
 
-  React.useEffect(() => {
+  useEffect(() => {
     const handleResize = () => {
       setIsSmallScreen(window.innerWidth < 992);
     };
     window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
-
-  const { t } = useTranslation();
 
   const currentPath = window.location.pathname;
 
   return (
-    <nav
-      className="navbar dark-blue navbar-expand-md sticky-top py-3"
+    <Navbar
+      collapseOnSelect
+      expand="md"
+      sticky="top"
+      variant="dark-blue"
+      className="navbar py-3"
       id="mainNav"
     >
-      <div className="container">
-        <Link className="navbar-brand d-flex align-items-center" to="/">
-          <span className="bs-icon-sm bs-icon-circle bs-icon-primary shadow d-flex justify-content-center align-items-center me-2 bs-icon">
-            <img
-              src="assets/img/HLS.png"
-              width="50"
-              height="50"
-              alt="main-logo"
-            />
-          </span>
-        </Link>
-        <button
-          data-bs-toggle="collapse"
-          className="navbar-toggler"
-          data-bs-target="#navcol-1"
+      <Container>
+        <Navbar.Brand
+          as={Link}
+          to="/"
+          className="bs-icon-sm bs-icon-circle bs-icon-primary shadow d-flex justify-content-center align-items-center me-2 bs-icon"
         >
-          <span className="visually-hidden">Toggle navigation</span>
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navcol-1">
-          <ul className="navbar-nav mx-auto">
-            <li className="nav-item">
-              <Link
-                className={`nav-link ${currentPath === "/" ? "active" : ""}`}
-                to="/"
-              >
-                {t("menu.home")}
-              </Link>
-            </li>
-            {!isSmallScreen && (
-              <li className="nav-item dropdown-hover">
-                <div
-                  className="nav-item dropdown"
-                  style={{ padding: "8px 0px", height: 41 }}
+          <img
+            src="assets/img/HLS.png"
+            width="50"
+            height="50"
+            alt="main-logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          className="custom-toggle"
+          style={{ borderColor: "white" }}
+        />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <>
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item">
+                <Link
+                  className={`nav-link ${currentPath === "/" ? "active" : ""}`}
+                  to="/"
                 >
-                  {/* eslint-disable-next-line */}
-                  <a
-                    className={`dropdown-toggle link-light ${
-                      currentPath.startsWith("/one_year_american")
-                        ? "active"
-                        : ""
-                    }`}
-                    style={{ cursor: "pointer" }}
+                  {t("menu.home")}
+                </Link>
+              </li>
+              {!isSmallScreen && (
+                <li className="nav-item dropdown-hover">
+                  <div
+                    className="nav-item dropdown"
+                    style={{ padding: "8px 0px", height: 41 }}
                   >
-                    {t("menu.programs")}
-                  </a>
-                  <div className="dropdown-menu" style={{ fontWeight: "bold" }}>
+                    {/* eslint-disable-next-line */}
+                    <a
+                      className={`dropdown-toggle link-light ${
+                        currentPath.startsWith("/one_year_american")
+                          ? "active"
+                          : ""
+                      }`}
+                      style={{ cursor: "pointer" }}
+                    >
+                      {t("menu.programs")}
+                    </a>
+                    <div
+                      className="dropdown-menu"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      <Link
+                        className="dropdown-item"
+                        to="/one_year_american"
+                        style={{ color: "black" }}
+                      >
+                        {t("menu.programs_list.one_year")}
+                      </Link>
+                      <Link
+                        className="dropdown-item"
+                        to="/one_week_stage"
+                        style={{ color: "black" }}
+                      >
+                        {t("menu.programs_list.prepa_eng")}
+                      </Link>
+                    </div>
+                  </div>
+                </li>
+              )}
+              {isSmallScreen && (
+                <>
+                  <li className="nav-item">
                     <Link
-                      className="dropdown-item"
+                      className={`nav-link ${
+                        currentPath === "/one_year_american" ? "active" : ""
+                      }`}
                       to="/one_year_american"
-                      style={{ color: "black" }}
                     >
                       {t("menu.programs_list.one_year")}
                     </Link>
+                  </li>
+                  <li className="nav-item">
                     <Link
-                      className="dropdown-item"
+                      className={`nav-link ${
+                        currentPath === "/one_week_stage" ? "active" : ""
+                      }`}
                       to="/one_week_stage"
-                      style={{ color: "black" }}
                     >
                       {t("menu.programs_list.prepa_eng")}
                     </Link>
-                  </div>
-                </div>
+                  </li>
+                </>
+              )}
+              <li className="nav-item">
+                <Link
+                  className={`nav-link ${
+                    currentPath === "/testimony" ? "active" : ""
+                  }`}
+                  to="/testimony"
+                >
+                  {t("menu.testiomonials")}
+                </Link>
               </li>
-            )}
-            {isSmallScreen && (
-              <>
-                <li className="nav-item">
-                  <Link
-                    className={`nav-link ${
-                      currentPath === "/one_year_american" ? "active" : ""
-                    }`}
-                    to="/one_year_american"
-                  >
-                    {t("menu.programs_list.one_year")}
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className={`nav-link ${
-                      currentPath === "/one_week_stage" ? "active" : ""
-                    }`}
-                    to="/one_week_stage"
-                  >
-                    {t("menu.programs_list.prepa_eng")}
-                  </Link>
-                </li>
-              </>
-            )}
-            <li className="nav-item">
-              <Link
-                className={`nav-link ${
-                  currentPath === "/testimony" ? "active" : ""
-                }`}
-                to="/testimony"
-              >
-                {t("menu.testiomonials")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                className={`nav-link ${
-                  currentPath === "/about" ? "active" : ""
-                }`}
-                to="/about"
-              >
-                {t("menu.about")}
-              </Link>
-            </li>
-            <li className="nav-item language-toggle">
-              <LanguageToggle />
-            </li>
-          </ul>
+              <li className="nav-item">
+                <Link
+                  className={`nav-link ${
+                    currentPath === "/about" ? "active" : ""
+                  }`}
+                  to="/about"
+                >
+                  {t("menu.about")}
+                </Link>
+              </li>
+              <li className="nav-item language-toggle">
+                <LanguageToggle />
+              </li>
+            </ul>
 
-          <Link className="btn btn-primary shadow" role="button" to="/contact">
-            {t("menu.contact")}
-          </Link>
-        </div>
-      </div>
-    </nav>
+            <Link
+              className="btn btn-primary shadow"
+              role="button"
+              to="/contact"
+            >
+              {t("menu.contact")}
+            </Link>
+          </>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
